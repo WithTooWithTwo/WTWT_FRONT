1186b44a9da8941786b357c8d551b7a08dc33f9f
Yuna Kim <kimyuna521@gmail.com>
2023-05-24 23:52:09 +0900

    [feat] SignUp 성별 선택 추가

--- wtwt/src/pages/SignUp.tsx
+++ wtwt/src/pages/SignUp.tsx
@@ -1,7 +1,8 @@
 import React, {useCallback, useRef, useState} from 'react';
 import {
-    Alert,
-    Pressable,
+    ActivityIndicator,
+    Alert, FlatList,
+    Pressable, ScrollView,
     StyleSheet,
     Text,
     TextInput,
@@ -11,12 +12,17 @@ import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {RootStackParamList} from '../../App';
 import DismissKeyboardView from '../components/DismissKeyboardView';
+import axios, {AxiosError} from "axios";
+import Config from "react-native-config";
+import {RadioButton} from "react-native-paper";
 
 type SignUnScreenProps = NativeStackScreenProps<RootStackParamList, 'SignUp'>;
 
 function SignIn({navigation}: SignUnScreenProps) {
+    const [loading, setLoading] = useState(false);
     const [name, setName] = useState('');
     const [nickname, setNickname] = useState('');
+    const [gender, setGender] = useState('MALE');
     const [phone, setPhone] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
@@ -49,11 +55,11 @@ function SignIn({navigation}: SignUnScreenProps) {
         setCheckPwd(text.trim());
     }, []);
 
-    const toSignIn = useCallback(() => {
-        navigation.navigate('SignIn');
-    }, [navigation]);
 
-    const onSubmit = useCallback(() => {
+    const onSubmit = useCallback(async () => {
+        if(loading){
+            return;
+        }
         if (!name || !name.trim()) {
             return Alert.alert('알림', '이름을 입력하세요');
         }
@@ -91,14 +97,34 @@ function SignIn({navigation}: SignUnScreenProps) {
             return Alert.alert('알림', '비밀번호가 일치하지 않습니다');
         }
         console.log(name, nickname, phone, email, password, checkPwd);
-        Alert.alert('알림', '회원가입 되었습니다.');
-        toSignIn();
-    }, [name, nickname, phone, email, password, checkPwd]);
+
+        try{
+            setLoading(true);
+            console.log(Config.API_URL);
+            const response = await axios.post(`${Config.API_URL}`, {
+                name, phone, email, password, nickname},
+                //{headers : {}}
+            )
+            console.log(response);
+            Alert.alert('알림', '회원가입 되었습니다.');
+            navigation.navigate('SignIn');
+        } catch (error){
+            const errorResponse = (error as AxiosError).response;
+            console.error();
+            if(errorResponse){
+                Alert.alert('알림', (errorResponse.data as any).data.massage);
+            }
+        } finally {
+            setLoading(false);
+        }
+
+    }, [loading, name, nickname, phone, email, password, checkPwd, navigation]);
 
     const canGoNext = name && nickname && phone && email && password && checkPwd;
 
     return (
         <DismissKeyboardView style={styles.container}>
+            <ScrollView>
             <View style={styles.inputZone}>
                 <View style={styles.textGroup}>
                     <Text style={styles.textLabel}>이름</Text>
@@ -142,6 +168,24 @@ function SignIn({navigation}: SignUnScreenProps) {
                         clearButtonMode="while-editing"
                     />
                 </View>
+                <View style={styles.textGroup}>
+                    <Text style={styles.textLabel}>성별</Text>
+                    <View style={styles.radio}>
+                            <RadioButton value="MALE"
+                                         status={ gender === 'MALE' ? 'checked' : "unchecked"}
+                                         onPress={() => setGender('MALE')} />
+                            <Text style={styles.radioText}>남자</Text>
+                            <RadioButton value="FEMALE"
+                                         status={ gender === 'FEMALE' ? 'checked' : "unchecked"}
+                                         onPress={() => setGender('FEMALE')} />
+                            <Text style={styles.radioText}>여자</Text>
+                            <RadioButton value="HIDE"
+                                         status={ gender === 'HIDE' ? 'checked' : "unchecked"}
+                                         onPress={() => setGender('HIDE')} />
+                            <Text style={styles.radioText}>선택 안함</Text>
+
+                    </View>
+                    </View>
                 <View style={styles.textGroup}>
                     <Text style={styles.textLabel}>휴대폰 번호</Text>
                     <TextInput
@@ -191,11 +235,11 @@ function SignIn({navigation}: SignUnScreenProps) {
                         autoCapitalize="none"
                         placeholder="비밀번호를 입력해주세요"
                         onChangeText={onChangePassword}
-                        // secureTextEntry
+                        secureTextEntry
                         returnKeyType="next"
                         importantForAutofill="yes"
                         autoComplete="password"
-                        // textContentType={'oneTimeCode'}
+                        textContentType={'oneTimeCode'}
                         blurOnSubmit={false}
                         ref={passwordRef}
                         onSubmitEditing={() => {
@@ -213,12 +257,12 @@ function SignIn({navigation}: SignUnScreenProps) {
                         placeholder="비밀번호를 다시 한 번 입력해주세요"
                         onChangeText={onChangeCheckPwd}
                         autoCapitalize="none"
-                        // secureTextEntry
+                        secureTextEntry
                         blurOnSubmit={false}
                         returnKeyType="send"
                         importantForAutofill="yes"
                         autoComplete="password"
-                        textContentType="password"
+                        textContentType={'oneTimeCode'}
                         ref={checkPwdRef}
                         clearButtonMode="while-editing"
                         onSubmitEditing={onSubmit}
@@ -233,10 +277,13 @@ function SignIn({navigation}: SignUnScreenProps) {
                             ? styles.loginButton
                             : [styles.loginButton, styles.loginButtonActive]
                     }
-                    disabled={!canGoNext}>
-                    <Text style={styles.loginButtonText}>회원가입 하기</Text>
+                    disabled={!canGoNext || loading}>
+                    {loading ? <ActivityIndicator color="blue" /> :
+                        <Text style={styles.loginButtonText}>회원가입 하기</Text>
+                    }
                 </Pressable>
             </View>
+            </ScrollView>
         </DismissKeyboardView>
     );
 }
@@ -247,24 +294,14 @@ const styles = StyleSheet.create({
         backgroundColor: 'white',
         flex: 1,
     },
-    logo: {
-        height: 270,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    logoText: {
-        padding: 30,
-        backgroundColor: '#D9D9D9',
-    },
-
     inputZone: {
         marginBottom: 0,
     },
     textGroup: {
-        marginBottom: 15,
+        marginBottom: 20,
     },
     textLabel: {
-        paddingBottom: 5,
+        paddingBottom: 7,
         color: '#707070',
     },
     textInput: {
@@ -287,6 +324,16 @@ const styles = StyleSheet.create({
     loginButtonActive: {
         backgroundColor: 'skyblue',
     },
+    radio: {
+        flexDirection: "row",
+        paddingVertical : 3
+    },
+    radioText: {
+        paddingTop: 10,
+        paddingHorizontal: 7,
+        marginRight: 5,
+        color: '#707070'
+    }
 });
 
 export default SignIn;
