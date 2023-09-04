5ea32f3d1c6a768d6402f1b8983c961b3f019a8b
yui880 <kimyuna521@gmail.com>
2023-09-04 15:20:19 +0900

    [feat] ChatRoom 화면 제작 (메시지 전송, 받아오기)

--- WithTooWithTwo/src/screens/Chat/ChatRoomScreen.tsx
+++ WithTooWithTwo/src/screens/Chat/ChatRoomScreen.tsx
@@ -1,14 +1,21 @@
-import {View} from 'react-native';
+import {SafeAreaView, StyleProp, Text, View, ViewStyle} from 'react-native';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import {HomeStackParamList} from '../Authenticated/HomeScreen';
 import {RouteProp} from '@react-navigation/native';
 import {ChatStackParamList} from '../Authenticated/ChatScreen';
-import {useCallback, useEffect, useRef, useState} from 'react';
+import React, {
+  CSSProperties,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 import {GiftedChat, IMessage} from 'react-native-gifted-chat';
 import io, {Socket} from 'socket.io-client';
 import {useSelector} from 'react-redux';
 import {RootState} from '../../store/store';
 import useSocket from '../../util/useSocket';
+import {fetchChatMessage} from '../../util/chat';
 
 type ChatRoomScreenNavigationProp = NativeStackNavigationProp<
   ChatStackParamList,
@@ -21,10 +28,11 @@ type ChatRoomScreenProps = {
   route: ChatRoomScreenRouteProp;
 };
 
-type MessageType = {
+export type MessageType = {
   roomId: string;
   sender: string;
   message: string;
+  sentAt?: string;
 };
 
 const SOCKET_URL = 'ws://3.39.87.78:8080/ws/chat';
@@ -52,14 +60,21 @@ const ChatRoomScreen = ({navigation, route}: ChatRoomScreenProps) => {
     newSocket.onmessage = event => {
       // Handle incoming messages from the WebSocket server
       const message = JSON.parse(event.data);
-      //  console.log(message);
+      console.log(message, 'message');
       const chatMessage: IMessage = {
-        _id: Math.random().toString(),
-        ...message,
+        _id: `${message.sendAt}-${Math.random()}`,
+        text: message.message,
+        createdAt: new Date(message.sendAt),
+        user: {
+          _id: message.sender,
+          name: 'You',
+        },
       };
+      console.log(chatMessage, 'chatMessage');
 
       // Add the chat message to the messages state
-      setMessages(prevMessages => [...prevMessages, chatMessage]);
+      //setMessages(prevMessages => [...prevMessages, chatMessage]);
+
       //setMessages(prevMessages => [...prevMessages, message]);
     };
 
@@ -78,6 +93,10 @@ const ChatRoomScreen = ({navigation, route}: ChatRoomScreenProps) => {
   }, []);
 
   useEffect(() => {
+    fetchChatMessage(roomId).then(res => setMessages(res));
+  }, [userId]);
+
+  useEffect(() => {
     console.log(messages);
   }, [messages]);
 
@@ -89,15 +108,57 @@ const ChatRoomScreen = ({navigation, route}: ChatRoomScreenProps) => {
         message: newMessages[0].text,
       };
       socket.send(JSON.stringify(message));
+      setMessages(previousMessages =>
+        GiftedChat.append(previousMessages, newMessages),
+      );
     }
   };
 
   return (
-    <GiftedChat
-      messages={messages}
-      onSend={newMessages => onSend(newMessages)}
-      user={{_id: userId, name: 'You'}}
-    />
+    <SafeAreaView style={{flex: 1}}>
+      <GiftedChat
+        messages={messages}
+        onSend={newMessages => onSend(newMessages)}
+        user={{_id: userId, name: 'You'}}
+        scrollToBottom={true}
+        alwaysShowSend={true}
+        // renderMessage={props => {
+        //   const {currentMessage} = props;
+        //   const isMyMessage = currentMessage!.user._id === userId;
+        //
+        //   return (
+        //     <MessageBubble
+        //       {...props}
+        //       currentMessage={currentMessage}
+        //       position={isMyMessage ? 'right' : 'left'} // Adjust position based on the user
+        //     />
+        //   );
+        // }}
+      />
+    </SafeAreaView>
+  );
+};
+
+const MessageBubble = ({
+  currentMessage,
+  position,
+}: {
+  currentMessage: any;
+  position: any;
+}) => {
+  const messageStyle: StyleProp<ViewStyle> = {
+    backgroundColor: position === 'right' ? '#007AFF' : '#ECECEC', // Adjust the colors as needed
+    borderRadius: 20,
+    padding: 10,
+    marginLeft: position === 'left' ? 0 : 50, // Add spacing for right-side messages
+    marginRight: position === 'right' ? 0 : 50, // Add spacing for left-side messages
+    alignSelf: position === 'right' ? 'flex-end' : 'flex-start', // Align to right or left
+  };
+
+  return (
+    <View style={messageStyle}>
+      <Text>{currentMessage.text}</Text>
+    </View>
   );
 };
 
--- WithTooWithTwo/src/util/chat.tsx
+++ WithTooWithTwo/src/util/chat.tsx
@@ -1,5 +1,7 @@
 import {GroupMember} from './group';
 import axios from 'axios';
+import {MessageType} from '../screens/Chat/ChatRoomScreen';
+import {IMessage} from 'react-native-gifted-chat';
 
 const URL = 'http://3.39.87.78:8080';
 
@@ -21,4 +23,24 @@ export const fetchChatList = async () => {
   const response = await axios.get(URL + '/chat');
   const chatList: ChatListType[] = response.data;
   return chatList;
+};
+
+export const fetchChatMessage = async (api: string = '') => {
+  const response = await axios.get(URL + '/chat/' + api);
+  const messages: IMessage[] = [];
+  const data = response.data.messages;
+  for (const key in data) {
+    const messageObj = {
+      _id: data[key].sendAt + Math.random().toString(),
+      text: data[key].message,
+      createdAt: new Date(data[key].sendAt),
+      user: {
+        _id: data[key].sender,
+        name: data[key].sender + data[key].sendAt,
+      },
+    };
+    messages.push(messageObj);
+  }
+
+  return messages.reverse();
 };
