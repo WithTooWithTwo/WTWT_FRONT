0570b05c1eec81cea27bf96b3011cd3d95d44fba
yui880 <kimyuna521@gmail.com>
2023-10-02 17:29:44 +0900

    [refactor] flatlist로 채팅방 스크롤 되도록 수정

--- WithTooWithTwo/src/screens/Chat/ChatListScreen.tsx
+++ WithTooWithTwo/src/screens/Chat/ChatListScreen.tsx
@@ -1,4 +1,5 @@
-import {SafeAreaView, View} from 'react-native';
+import React from 'react';
+import {FlatList, SafeAreaView, View} from 'react-native';
 import ScreenHeader from '../../components/UI/ScreenHeader';
 import {useEffect, useState} from 'react';
 import {ChatListType, fetchChatList} from '../../util/chat';
@@ -16,14 +17,17 @@ const ChatListScreen = () => {
     <SafeAreaView style={{backgroundColor: 'white', flex: 1}}>
       <ScreenHeader color={'white'} isGoBack={false} title="채팅" />
       <View style={{backgroundColor: Colors.grey1, padding: 20}}>
-        {chatList &&
-          chatList.map(item => (
-            <ChatListItem key={item.title + item.lastMessage} chat={item} />
-          ))}
+        <FlatList
+          data={chatList}
+          renderItem={({item}) => <ChatListItem chat={item} />}
+          keyExtractor={item => item.title + item.lastMessage}
+          windowSize={2}
+          showsVerticalScrollIndicator={false}
+        />
       </View>
       <View style={{backgroundColor: Colors.grey1, flex: 1}} />
     </SafeAreaView>
   );
 };
 
-export default ChatListScreen;
+export default React.memo(ChatListScreen);
