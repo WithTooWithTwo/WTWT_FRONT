837c18a67bfaee90ae4438964a90c21c6eed3d65
yui880 <kimyuna521@gmail.com>
2023-10-02 15:43:29 +0900

    [feat] 탭바 아이콘, 레이블 변경

--- WithTooWithTwo/App.tsx
+++ WithTooWithTwo/App.tsx
@@ -1,4 +1,4 @@
-import {View} from 'react-native';
+import {Text, View} from 'react-native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import LoginScreen from './src/screens/Root/LoginScreen';
@@ -8,13 +8,19 @@ import ChatScreen from './src/screens/Authenticated/ChatScreen';
 import GroupScreen from './src/screens/Authenticated/GroupScreen';
 import MyPageScreen from './src/screens/Authenticated/MyPageScreen';
 import {RootState, store} from './src/store/store';
-import {NavigationContainer} from '@react-navigation/native';
+import {
+  getFocusedRouteNameFromRoute,
+  NavigationContainer,
+} from '@react-navigation/native';
 import {Provider, useDispatch, useSelector} from 'react-redux';
 import {authenticate, logout} from './src/slices/authSlice';
 import {useEffect} from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import RNEventSource from 'react-native-event-source';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
 export type RootStackParamList = {
   Login: undefined;
@@ -57,25 +63,75 @@ const AuthenticatedTabNavigator = () => {
         options={{
           headerShown: false,
           tabBarIcon: ({size, focused}) => {
-            return <Ionicons name="home-sharp" size={15} color={'#3C70FF'} />;
+            return (
+              <Ionicons
+                name="home-sharp"
+                size={17}
+                color={focused ? '#3C70FF' : 'grey'}
+              />
+            );
           },
+          tabBarLabel: '홈',
           unmountOnBlur: true,
+          tabBarLabelStyle: {fontSize: 11},
         }}
       />
       <AuthenticatedTab.Screen
         name="Chat"
         component={ChatScreen}
-        options={{headerShown: false, unmountOnBlur: true}}
+        options={{
+          headerShown: false,
+          unmountOnBlur: true,
+          tabBarIcon: ({size, focused}) => {
+            return (
+              <Ionicons
+                name="chatbox-ellipses"
+                size={17}
+                color={focused ? '#3C70FF' : 'grey'}
+              />
+            );
+          },
+          tabBarLabel: '채팅',
+          tabBarLabelStyle: {fontSize: 11},
+        }}
       />
       <AuthenticatedTab.Screen
         name="Group"
         component={GroupScreen}
-        options={{headerShown: false, unmountOnBlur: true}}
+        options={{
+          headerShown: false,
+          unmountOnBlur: true,
+          tabBarIcon: ({size, focused}) => {
+            return (
+              <AntDesign
+                name="appstore1"
+                size={17}
+                color={focused ? '#3C70FF' : 'grey'}
+              />
+            );
+          },
+          tabBarLabel: '그룹',
+          tabBarLabelStyle: {fontSize: 11},
+        }}
       />
       <AuthenticatedTab.Screen
         name="MyPage"
         component={MyPageScreen}
-        options={{headerShown: false, unmountOnBlur: true}}
+        options={{
+          headerShown: false,
+          unmountOnBlur: true,
+          tabBarIcon: ({size, focused}) => {
+            return (
+              <FontAwesome5
+                name="user-alt"
+                size={17}
+                color={focused ? '#3C70FF' : 'grey'}
+              />
+            );
+          },
+          tabBarLabel: '마이페이지',
+          tabBarLabelStyle: {fontSize: 11},
+        }}
       />
     </AuthenticatedTab.Navigator>
   );
@@ -95,7 +151,7 @@ const Navigation = () => {
     fetchToken();
   }, []);
 
-  // dispatch(logout());
+  //dispatch(logout());
 
   return (
     <NavigationContainer>
