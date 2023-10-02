1262b46217ef2aa2afb4bb44ca9998bb9d9e5803
yui880 <kimyuna521@gmail.com>
2023-10-02 15:39:52 +0900

    [feat] 알림 수락 기능 구현

--- WithTooWithTwo/src/components/Notice/InvitationItem.tsx
+++ WithTooWithTwo/src/components/Notice/InvitationItem.tsx
@@ -1,19 +1,35 @@
 import {Pressable, StyleSheet, Text, View} from 'react-native';
+import {acceptInvitation, AlarmType} from '../../util/group';
+import {detailDate} from '../../util/date';
+import {Colors} from '../../constants/styles';
 
-function InvitationItem() {
+function InvitationItem({
+  alarm,
+  setReload,
+}: {
+  alarm: AlarmType;
+  setReload: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
+  const pressAcceptButton = () => {
+    acceptInvitation(alarm.id.toString()).then(r => {
+      console.log(r);
+      setReload(prevState => !prevState);
+    });
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.titleBox}>
         <Text style={styles.title}>그룹 초대</Text>
-        <Text style={styles.views}>10시간 전</Text>
+        <Text style={styles.views}>{detailDate(alarm.createdAt)}</Text>
       </View>
-      <Text style={styles.content}>000님의 독일 여행팟</Text>
+      <Text style={styles.content}>{alarm.message.slice(7)}</Text>
       <View style={styles.buttonBox}>
         <Pressable style={styles.button}>
           <Text>거절하기</Text>
         </Pressable>
-        <Pressable style={styles.button}>
-          <Text>수락하기</Text>
+        <Pressable style={styles.acceptButton} onPress={pressAcceptButton}>
+          <Text style={{color: 'white'}}>수락하기</Text>
         </Pressable>
       </View>
     </View>
@@ -23,9 +39,12 @@ function InvitationItem() {
 const styles = StyleSheet.create({
   container: {
     justifyContent: 'center',
-    paddingVertical: 20,
     paddingHorizontal: 25,
+    paddingTop: 20,
+    paddingBottom: 25,
     marginTop: 10,
+    borderBottomColor: Colors.grey5,
+    borderBottomWidth: 1,
   },
   titleBox: {
     flexDirection: 'row',
@@ -45,6 +64,7 @@ const styles = StyleSheet.create({
   content: {
     fontWeight: '400',
     fontSize: 14,
+    paddingVertical: 3,
   },
   buttonBox: {
     flexDirection: 'row',
@@ -57,6 +77,28 @@ const styles = StyleSheet.create({
     paddingHorizontal: 45,
     backgroundColor: '#F8F8F9',
     borderRadius: 10,
+    shadowColor: 'black',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 2.22,
+    elevation: 3,
+  },
+  acceptButton: {
+    paddingVertical: 13,
+    paddingHorizontal: 45,
+    backgroundColor: Colors.primary500,
+    borderRadius: 10,
+    shadowColor: 'black',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 2.22,
+    elevation: 3,
   },
   buttonText: {
     fontWeight: '400',
