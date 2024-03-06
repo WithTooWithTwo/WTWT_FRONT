75be7daaae20e7f4a49ed3cf5fdbd88aff0190ea
yui880 <kimyuna521@gmail.com>
2024-03-06 23:22:39 +0900

    [refactor] api 연결 수정

--- WithTooWithTwo/src/util/chat.tsx
+++ WithTooWithTwo/src/util/chat.tsx
@@ -2,8 +2,6 @@ import {GroupMember} from './group';
 import axios from 'axios';
 import {IMessage} from 'react-native-gifted-chat';
 
-const URL = 'http://3.39.87.78:8080';
-
 export type ChatListType = {
   roomId: number;
   title: string;
@@ -13,19 +11,21 @@ export type ChatListType = {
 };
 
 export const requestChatRoom = async (postId: string) => {
-  const response = await axios.post(URL + '/chat?postId=' + postId);
+  const response = await axios.post(
+    process.env.API_KEY + '/chat?postId=' + postId,
+  );
   const chatRoom = response.data;
   return chatRoom;
 };
 
 export const fetchChatList = async () => {
-  const response = await axios.get(URL + '/chat');
+  const response = await axios.get(process.env.API_KEY + '/chat');
   const chatList: ChatListType[] = response.data;
   return chatList;
 };
 
 export const fetchChatMessage = async (api: string = '') => {
-  const response = await axios.get(URL + '/chat/' + api);
+  const response = await axios.get(process.env.API_KEY + '/chat/' + api);
   const messages: IMessage[] = [];
   const data = response.data.messages;
   for (const key in data) {
--- WithTooWithTwo/src/util/review.tsx
+++ WithTooWithTwo/src/util/review.tsx
@@ -1,15 +1,15 @@
 import axios from 'axios';
 import {ReviewType} from '../screens/Group/GroupReviewScreen';
 
-const API_KEY = 'http://3.39.87.78:8080';
-
 export type OptionType = {
   id: number;
   name: string;
 };
 
 export async function fetchReviewOptions(api: string = '') {
-  const response = await axios.get(API_KEY + '/review-options' + api);
+  const response = await axios.get(
+    process.env.API_KEY + '/review-options' + api,
+  );
   let options = null;
 
   for (const key in response.data) {
@@ -20,7 +20,7 @@ export async function fetchReviewOptions(api: string = '') {
 
 export const sendReview = async (id: string, reviews: ReviewType[]) => {
   const response = await axios.post(
-    'http://3.39.87.78:8080' + '/reviews/' + id,
+    process.env.API_KEY + '/reviews/' + id,
     reviews,
   );
   const result = response.data.result;
@@ -28,7 +28,7 @@ export const sendReview = async (id: string, reviews: ReviewType[]) => {
 };
 
 export async function storePosts(posts: any, contentType: string) {
-  const response = await axios.post(URL + '/posts', posts, {
+  const response = await axios.post(process.env.API_KEY + '/posts', posts, {
     headers: {'content-type': contentType || ''},
   });
   const id = response.data.id;
